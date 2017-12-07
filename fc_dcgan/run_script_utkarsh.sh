0a73be181a9156aa0b3cbf07a061858922bf93af
utkarsh1404 <utkarsh1404@gmail.com>
2017-12-07 16:35:47 -0500

    abcd

--- fc_dcgan/run_script_utkarsh.sh
+++ fc_dcgan/run_script_utkarsh.sh
@@ -1,4 +1,4 @@
-python mnist_model_FC.py --num_epochs=51 --loss_func=0 --run=fc_0
-python mnist_model_FC.py --num_epochs=51 --lr=0.00005 --loss_func=0 --run=fc_1
-python mnist_model_FC.py --num_epochs=51 --layer_list 256 512 1024 4196 16748 --batch_size=200 --loss_func=0 --run=fc_2
-python mnist_model_FC.py --num_epochs=51 --layer_list 256 512 1024 4196 16748 --batch_size=200 --lr=0.00005 --loss_func=0 --run=fc_3
+#python mnist_model_FC.py --num_epochs=51 --loss_func=0 --run=fc_0
+#python mnist_model_FC.py --num_epochs=51 --lr=0.00005 --loss_func=0 --run=fc_1
+python mnist_model_FC.py --num_epochs=51 --layer_list 512 1024 2048 4096 8192 --batch_size=200 --loss_func=0 --run=fc_2
+#python mnist_model_FC.py --num_epochs=51 --layer_list 512 1024 2048 4096 8192 --batch_size=200 --lr=0.00005 --loss_func=0 --run=fc_3
