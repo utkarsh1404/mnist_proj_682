a7d3a74c900916fca5ff4b0bf41cd04c669c22ea
utkarsh1404 <utkarsh1404@gmail.com>
2017-12-07 11:10:42 -0500

    abcd

--- dcgan/run_script_kruti.sh
+++ dcgan/run_script_kruti.sh
@@ -1,5 +1,5 @@
-python mnist_model_dcgan.py --num_epochs=53 --loss_func=1 --run=ce_0 > run_ce_0.txt
-python mnist_model_dcgan.py --num_epochs=53 --lr=0.002 --loss_func=1 --run=ce_1 > run_ce_1.txt
-#python mnist_model_dcgan.py --num_epochs=53 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --batch_size=256 --loss_func=1 --run=ce_2 > run_ce_2.txt
-#python mnist_model_dcgan.py --num_epochs=53 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --lr=0.002 --batch_size=256 --loss_func=1 --run=ce_3 > run_ce_3.txt
+python mnist_model_dcgan.py --num_epochs=51 --loss_func=1 --run=ce_0
+python mnist_model_dcgan.py --num_epochs=51 --lr=0.00005 --loss_func=1 --run=ce_1
+#python mnist_model_dcgan.py --num_epochs=51 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --batch_size=256 --loss_func=1 --run=ce_2
+#python mnist_model_dcgan.py --num_epochs=51 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --lr=0.00005 --batch_size=256 --loss_func=1 --run=ce_3
 
--- dcgan/run_script_utkarsh.sh
+++ dcgan/run_script_utkarsh.sh
@@ -1,5 +1,5 @@
-python mnist_model_dcgan.py --num_epochs=53 --loss_func=0 --run=ls_0 > run_ls_0.txt
-python mnist_model_dcgan.py --num_epochs=53 --lr=0.002 --loss_func=0 --run=ls_1 > run_ls_1.txt
-#python mnist_model_dcgan.py --num_epochs=53 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --batch_size=256 --loss_func=0 --run=ls_2 > run_ls_2.txt
-#python mnist_model_dcgan.py --num_epochs=53 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --lr=0.002 --batch_size=256 --loss_func=0 --run=ls_3 > run_ls_3.txt
+python mnist_model_dcgan.py --num_epochs=51 --loss_func=0 --run=ls_0
+python mnist_model_dcgan.py --num_epochs=51 --lr=0.00005 --loss_func=0 --run=ls_1
+#python mnist_model_dcgan.py --num_epochs=51 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --batch_size=256 --loss_func=0 --run=ls_2
+#python mnist_model_dcgan.py --num_epochs=51 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --lr=0.00005 --batch_size=256 --loss_func=0 --run=ls_3
 
