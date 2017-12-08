f7f5d123621249ada21b078639c9f59c7b4911e0
utkarsh1404 <utkarsh1404@gmail.com>
2017-12-07 23:00:22 -0500

    abcd

--- dcgan/run_script_kruti.sh
+++ dcgan/run_script_kruti.sh
@@ -1,5 +1,5 @@
 python mnist_model_dcgan.py --num_epochs=45 --loss_func=1 --run=ce_0
 python mnist_model_dcgan.py --num_epochs=45 --lr=0.00005 --loss_func=1 --run=ce_1
-python mnist_model_dcgan.py --num_epochs=45 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --batch_size=200 --loss_func=1 --run=ce_2
-#python mnist_model_dcgan.py --num_epochs=45 --layer_list 512 256 --fclayer_list 2000 5000 --filter_sz=3 --lr=0.00005 --batch_size=200 --loss_func=1 --run=ce_3
+python mnist_model_dcgan.py --num_epochs=45 --layer_list 256 128 --fclayer_list 1024 2048 --batch_size=200 --loss_func=1 --run=ce_2
+#python mnist_model_dcgan.py --num_epochs=45 --layer_list 256 128 --fclayer_list 1024 2048 --lr=0.00005 --batch_size=200 --loss_func=1 --run=ce_3
 
