d67a5f48358c0f61db99e233b374d51dd60b6c98
kruti3 <kruti.chauhan.93@gmail.com>
2017-12-10 15:26:49 -0500

    scripts

---
+++ infogan/run_script_utkarsh.sh
@@ -0,0 +1,5 @@
+python mnist_model_infogan.py --num_epochs=50  --run=in_0
+#python mnist_model_infogan.py --num_epochs=50 --angle=-0.9 --run=in_1
+python mnist_model_infogan.py --num_epochs=50 --angle=0.3 --lr1=1e-3 --lr2=7.5e-4 --run=in_2
+#python mnist_model_infogan.py --num_epochs=50 --angle=-0.3 --lr1=1e-3 --lr2=7.5e-4 --run=in_3
+
